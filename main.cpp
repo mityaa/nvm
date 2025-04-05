2818bef489df71be6ce86b63e0f16a0a1936d0a2
Nenilin_DY <Nenilin_DY@directum.com>
2025-04-05 20:55:49 +0300

    Inin commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+
+int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        std::cout << "Использование: утилита <аргумент>" << std::endl;
+        return 1;
+    }
+
+
+    std::string argument = argv[1];
+
+     std::cout << "Вы ввели аргумент: " << argument << std::endl;
+
+    return 0;
+}
